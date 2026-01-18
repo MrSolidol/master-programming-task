857b90ca26513990a617677ac6af17298b10ce77
Alexandr Kolesnikov <76955369+MrSolidol@users.noreply.github.com>
2026-01-18 15:14:18 +0000

    lab1 - done

--- lab1/include/introduction.hpp
+++ lab1/include/introduction.hpp
@@ -1,35 +1,30 @@
-/* Introduction class.
- * @file
- * @date 2021-09-04
- * @author Anonymous
- */
-
 #ifndef __INTRODUCTION_HPP__
 #define __INTRODUCTION_HPP__
 
 #include <string>
+#include <string_view>
 
-//{
 class introduction
 {
 public:
     std::string_view name() const
     {
-        return ...
+        return name_;
     }
 
     std::string_view surename() const
     {
-        return ...
+        return surename_;
     }
 
     std::string fullname() const
     {
-        return ...
+        return std::string{name_} + " " + std::string{surename_};
     }
 
 private:
+    std::string name_{"Alexander"};
+    std::string surename_{"Kolesnikov"};
 };
-//}
 
 #endif // __INTRODUCTION_HPP__
--- lab1/tests/introduction.cpp
+++ lab1/tests/introduction.cpp
@@ -1,9 +1,3 @@
-/* Test file for introduction task.
- * @file
- * @date 2021-09-04
- * @author Anonymous
- */
-
 #include <regex>
 
 #include <catch2/catch_all.hpp>
@@ -18,5 +12,4 @@ TEST_CASE("introduction")
     CHECK(std::regex_match(intr.name().data(), std::regex{R"([A-Z]\w+)"}));
     CHECK(std::regex_match(intr.surename().data(), std::regex{R"([A-Z]\w+)"}));
     CHECK(std::regex_match(intr.fullname(), std::regex{R"(\w+ \w+)"}));
-    // Alternate form is CHECK_THAT(intr.fullname(), Catch::Matchers::Matches(R"(\w+ \w+)"));
 }
